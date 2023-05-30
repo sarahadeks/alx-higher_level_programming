728dc46bcd44f8ca24daad90e5505dbef5664198
sarahadeks <sarahopeyemi@yahoo.com>
2023-05-30 15:45:34 -0700

    classesandobjects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A class for an empty square class"""
+
+
+class Square:
+    """Empty Square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class for that defines a Square"""
+
+
+class Square:
+    """Square class initialized with size"""
+
+    def __init__(self, size):
+        self.__size = size
