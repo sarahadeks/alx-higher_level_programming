ffb118c30d390e2c91f6eeeedcd77194588a03d7
sarahadeks <sarahopeyemi@yahoo.com>
2023-06-13 04:11:55 -0700

    final update

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This function prints a message with the first and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ Function that prints "My name is <first name> <last name>"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Function that prints 2 new lines after ".?:" characters
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each
+    of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    list_lines = [lines.strip(' ') for lines in text.split('\n')]
+    revised = "\n".join(list_lines)
+    print(revised, end="")
