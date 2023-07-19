1b2c1476d456a1685109f85cf7ad8971dc71d122
sarahadeks <sarahopeyemi@yahoo.com>
2023-07-18 17:33:25 -0700

    final updates

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- script that create a database
+--if the database already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- script that delete database in MySQL server
+-- if the database doesn't exist, your script should not fail
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
